89bacf34cb09a966a871bf6d1ba35174afa9dd1d
Elliot <ericliu11046126@gmail.com>
2018-05-28 10:53:45 +1200

    Add project

---
+++ GridCrudSorting/Controllers/ExerciseController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using GridCrudSorting.Models;
+
+namespace GridCrudSorting.Controllers
+{
+    public class ExerciseController : Controller
+    {
+        // GET: Exercise List
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<ExerciseRecord> exeList = db.ExerciseRecords.ToList<ExerciseRecord>();
+
+                return Json(new {data = exeList}, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View(new ExerciseRecord());
+        }
+
+        [HttpPost]
+        public ActionResult Add(ExerciseRecord obj)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.ExerciseRecords.Add(obj);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ExerciseAlreadyExists(string newName)
+        {
+            using (DBModel db = new DBModel())
+            {
+                var result =  db.ExerciseRecords.Any(x => x.ExerciseName == newName);
+                return Json(!result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            {
+                using (DBModel db = new DBModel())
+                {
+                    return View(db.ExerciseRecords.Where(x => x.Id == id).FirstOrDefault<ExerciseRecord>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ExerciseRecord obj)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Entry(obj).State = EntityState.Modified;
+                db.SaveChanges();
+                return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);               
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                ExerciseRecord obj = db.ExerciseRecords.Where(x => x.Id == id).FirstOrDefault<ExerciseRecord>();
+                db.ExerciseRecords.Remove(obj);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+} 
---
+++ GridCrudSorting/Scripts/MyFunctions/FunctionList.js
@@ -0,0 +1,68 @@
+﻿var Popup;
+
+function PopupForm(url) {
+
+    var formDiv = $('<div/>');
+
+    $.get(url)
+        .done(function (response) {
+
+            formDiv.html(response);
+
+            Popup = formDiv.dialog({
+                autoOpen: true,
+                resizable: false,
+                title: "Enter New Exercise Record",
+                height: 350,
+                width: 600,
+                close: function () {
+                    Popup.dialog('destroy').remove();
+                }
+            });
+        });
+}
+
+function SubmitForm(form) {
+
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: $(form).serialize(),
+            success: function (data) {
+                if (data.success) {
+                    Popup.dialog('close');
+                    dataTable.ajax.reload();
+                    $.notify(data.message, {
+                        globalPosition: "top center",
+                        className: "success"
+                    });
+                }
+            }
+        });
+    }
+    return false;
+}
+
+function Delete(id) {
+
+    if (confirm('Are you sure to delete this record?')) {
+
+        $.ajax({
+            type: "POST",
+            url: '@Url.Action("Delete","Exercise")/' + id,
+            success: function (data) {
+                if (data.success) {
+
+                    dataTable.ajax.reload();
+                    $.notify(data.message, {
+                        globalPosition: "top center",
+                        className: "Selected record successfully removed"
+                    });
+                }
+            }
+        });
+    }
+}  
+
